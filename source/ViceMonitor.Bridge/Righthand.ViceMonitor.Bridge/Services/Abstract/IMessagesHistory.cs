922a9cc9d7e05f4d8b7959ef9c53599a01750707
Miha Markič <miha@rthand.com>
2023-12-02 09:57:03 +0100

    Cleans up code

--- source/ViceMonitor.Bridge/Righthand.ViceMonitor.Bridge/Services/Abstract/IMessagesHistory.cs
+++ source/ViceMonitor.Bridge/Righthand.ViceMonitor.Bridge/Services/Abstract/IMessagesHistory.cs
@@ -38,3 +38,15 @@ namespace Righthand.ViceMonitor.Bridge.Services.Abstract
         void UpdateWithLinkedResponse(int id, ViceResponse response);
     }
 }
+
+/// <summary>
+/// Contains message history related data including command and response.
+/// </summary>
+/// <param name="Sequence">VICE sequence value.. Null when response is unbound.</param>
+/// <param name="Command">VICE command. Null when response is unbound.</param>
+/// <param name="Response">VICE response</param>
+/// <param name="StartTime">Ticks when command was issued.</param>
+/// <param name="Elapsed">Ticks when response was received. Null when response is unbound.</param>
+/// <param name="LinkedResponses">A list of linked responses (i.e. for <see cref="CheckpointInfoResponse"/>)</param>
+public record CommunicationData(uint? Sequence, IViceCommand? Command, ViceResponse? Response, long StartTime, long? Elapsed,
+    ImmutableArray<ViceResponse> LinkedResponses);
